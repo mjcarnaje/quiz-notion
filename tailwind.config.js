9b1431191787b40a7fb55fb0cae80e699d88970b
Michael James Carnaje <michaeljamescarnaje1@gmail.com>
2021-05-28 10:32:52 +0800

    purged unused tailwindcss a little more

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   purge: {
+    mode: "all",
+    preserveHtmlElements: false,
     enabled: true,
     content: ["./public/**/*.html", "./public/**/*.js"],
+    options: {
+      keyframes: true,
+    },
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
